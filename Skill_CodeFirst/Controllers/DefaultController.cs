e1b31bc0ee8798c6d7361fab70bb36d4033b8161
Doğukan ÖZEL <dogukanozel692@gmail.com>
2023-12-14 11:47:21 +0300

    Add project files.

---
+++ Skill_CodeFirst/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using Skill_CodeFirst.Models.siniflar;
+
+namespace Skill_CodeFirst.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            CONTEXT c = new CONTEXT();
+            var degerler = c.YETENEKLERS.ToList();
+            return View(degerler);
+        }
+    }
+}
